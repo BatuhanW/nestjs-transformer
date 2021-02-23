25a72a8b66b5af7ac88711755d7a864c90cf7584
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2021-02-23 07:57:02 +0300

    Initiate kafka

--- apps/nest-transformer/src/app.controller.ts
+++ apps/nest-transformer/src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { SubscribeTo } from '@adapters/kafka';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @SubscribeTo('test')
+  async test(payload: any) {
+    console.log(JSON.parse(payload).test)
   }
 }
--- apps/nest-transformer/src/app.module.ts
+++ apps/nest-transformer/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KafkaModule } from '@adapters/kafka';
 
 @Module({
-  imports: [],
+  imports: [
+    KafkaModule.register(
+      { clientId: 'test', brokers: ['localhost:9092'] },
+      { groupId: 'test' },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
