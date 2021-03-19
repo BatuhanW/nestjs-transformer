88faea972eff39e53ed8668c4ad01e80ad30f3d3
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2021-03-20 01:05:07 +0300

    Generic for filter

--- libs/kafka/src/interfaces.ts
+++ libs/kafka/src/interfaces.ts
@@ -7,5 +7,5 @@ export interface KafkaModuleRegisterOptions {
 
 export interface KafkaSubscriberDecoratorParams {
   topicName: string;
-  filter?: (message: Record<string, any>) => boolean;
+  filter?: <IncomingPayload extends Record<string, any>>(message: IncomingPayload) => boolean;
 }
