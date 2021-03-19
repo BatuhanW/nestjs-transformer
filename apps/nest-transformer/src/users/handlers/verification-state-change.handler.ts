5f264d548d61d62ee8aa8558e1efcdc0ca673b8d
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2021-03-19 22:54:25 +0300

    VerificationStateChangeHandler

--- apps/nest-transformer/src/users/handlers/verification-state-change.handler.ts
+++ apps/nest-transformer/src/users/handlers/verification-state-change.handler.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { KafkaSubscriber } from '@adapters/kafka';
 import { Handler, BaseHandler } from '@core';
+import { VerificationStateChangeTransformer } from '../transformers/verification-state-change.transformer';
+import { UserEnricher } from '../enrichers/user.enricher';
+import { AmplitudeAction } from '../actions/amplitude.action';
+import { BrazeAction } from '../actions/braze.action';
 
 const topics = [
   'verification_not_started',
@@ -15,4 +19,17 @@ const topics = [
   filter: (message) => topics.includes(message.event_name),
 })
 @Handler()
-export class VerificationStateChangeHandler extends BaseHandler {}
+export class VerificationStateChangeHandler extends BaseHandler {
+  constructor(
+    private transformer: VerificationStateChangeTransformer,
+    private enricher: UserEnricher,
+    private ampAction: AmplitudeAction,
+    private brazeAction: BrazeAction,
+  ) {
+    super();
+
+    this.transformers = [this.transformer];
+    this.enrichers = [this.enricher];
+    this.actions = [this.ampAction, this.brazeAction];
+  }
+}
