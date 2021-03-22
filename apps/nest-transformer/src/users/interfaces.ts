4f7df42c1e6a519c8e74a67352d954a064c065b1
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2021-03-22 22:22:26 +0300

    Update interface to be able to throw error

--- apps/nest-transformer/src/users/interfaces.ts
+++ apps/nest-transformer/src/users/interfaces.ts
@@ -1,16 +1,20 @@
 export interface TestDataPayload {
   test: string;
+  test2: {
+    test3: string;
+  };
 }
 
 export interface TestDataResult {
-  data: {
-    test: string;
-  };
+  data: TestDataPayload;
 }
 
 export interface EnrichedTestData {
   data: {
     test: string;
+    test2: {
+      test3: string;
+    };
     name: string;
     age: number;
     count: number;
