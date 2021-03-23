4d635c1ab60260d2dbc0a064585463b1e4859414
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2021-03-23 03:48:41 +0300

    Fix base destination type

--- libs/core/src/destination/base.destination.ts
+++ libs/core/src/destination/base.destination.ts
@@ -1,7 +1,7 @@
 import { DefaultObject } from '../types';
 
 export abstract class BaseDestination<Payload = DefaultObject> {
-  abstract perform(payload: Payload): void;
+  abstract perform(payload: Payload): void | Promise<void>;
 
   // eslint-disable-next-line
   onSuccess(): void | Promise<void> {}
