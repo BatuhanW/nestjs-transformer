f42928b45eedbfd8265efc4fcc9fe80473838c75
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2021-03-23 04:33:48 +0300

    Add onError hook to BaseDestination

--- libs/core/src/destination/base.destination.ts
+++ libs/core/src/destination/base.destination.ts
@@ -5,4 +5,13 @@ export abstract class BaseDestination<Payload = DefaultObject> {
 
   // eslint-disable-next-line
   onSuccess(): void | Promise<void> {}
+
+  onError(error: Error): void | Promise<void> {
+    console.dir({
+      level: 'ERROR',
+      timestamp: new Date().toISOString(),
+      class_name: this.constructor.name,
+      error,
+    });
+  }
 }
