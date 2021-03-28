7979126b9700bd75625dd01e4e99a8fdb1303f4a
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2021-03-28 11:47:35 +0300

    process exit on unhandler error or rejection

--- apps/nest-transformer/src/main.ts
+++ apps/nest-transformer/src/main.ts
@@ -5,6 +5,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  process.on('uncaughtException', (error) => {
+    console.error({ error });
+    process.exit(1);
+  });
+
+  process.on('unhandledRejection', (error) => {
+    console.error({ error });
+    process.exit(1);
+  });
+
   const configService = app.get(ConfigService);
 
   const port = configService.get('PORT') || 5757;
