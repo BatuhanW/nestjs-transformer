05497b754a86f3f9b8a551dabc646241b601460d
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2021-04-08 14:18:41 +0300

    Refactor core.handler

--- libs/core/src/handler/core.handler.ts
+++ libs/core/src/handler/core.handler.ts
@@ -5,17 +5,7 @@ export abstract class CoreHandler<Payload = AnyObject> {
   protected enricher?: BaseEnricher;
   protected destinations: { transformer?: BaseTransformer; destination: BaseDestination }[] = [];
 
-  /* eslint-disable @typescript-eslint/no-empty-function */
-  public onStart(_payload: Payload): void | Promise<void> {}
-  public onSuccess(): void | Promise<void> {}
-  /* eslint-enable @typescript-eslint/no-empty-function */
-
-  onError(error: Error): void | Promise<void> {
-    console.dir({
-      level: 'ERROR',
-      timestamp: new Date().toISOString(),
-      class_name: this.constructor.name,
-      error,
-    });
-  }
+  public onStart?(payload: Payload): void | Promise<void>;
+  public onError?(error: Error): void | Promise<void>;
+  public onSuccess?(): void | Promise<void>;
 }
