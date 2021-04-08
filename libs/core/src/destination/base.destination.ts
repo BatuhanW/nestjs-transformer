2f657ebc7d3261e9b286438264330c72d3875582
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2021-04-08 14:15:12 +0300

    Refactor base.destination

--- libs/core/src/destination/base.destination.ts
+++ libs/core/src/destination/base.destination.ts
@@ -1,17 +1,8 @@
 import { AnyObject } from '@core';
 
-export abstract class BaseDestination<Payload = AnyObject> {
+export abstract class BaseDestination<Payload extends AnyObject = AnyObject> {
   abstract perform(payload: Payload): Promise<void>;
 
-  // eslint-disable-next-line
-  onSuccess(): void | Promise<void> {}
-
-  onError(error: Error): void | Promise<void> {
-    console.dir({
-      level: 'ERROR',
-      timestamp: new Date().toISOString(),
-      class_name: this.constructor.name,
-      error,
-    });
-  }
+  public onError?(error: Error): void | Promise<void>;
+  public onSuccess?(): void | Promise<void>;
 }
