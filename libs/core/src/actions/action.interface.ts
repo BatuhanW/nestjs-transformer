ea8a853b8cde18c58cc9b85c64f9b48770db9097
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2021-04-09 23:14:23 +0300

    Add name to action interface

--- libs/core/src/actions/action.interface.ts
+++ libs/core/src/actions/action.interface.ts
@@ -2,6 +2,7 @@ import { BaseTransformer } from '@core/transformer';
 import { BaseDestination } from '@core/actions/base.destination';
 
 export interface Action {
+  name: string;
   transformer?: BaseTransformer;
   destination: BaseDestination;
 }
